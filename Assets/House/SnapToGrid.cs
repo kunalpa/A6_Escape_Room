4204726a1e49c58b78a2f9afd298293f10809157
ZhuohaoZeng <Zhuohao_Zeng@unc.edu>
2024-11-18 12:26:31 -0500

    revised camera and readded raygun

--- Assets/House/SnapToGrid.cs
+++ Assets/House/SnapToGrid.cs
@@ -10,19 +10,19 @@ public class SnapToGrid : MonoBehaviour
     public int tileSize = 1;
     public Vector3 tileOffset = Vector3.zero;
     // Update is called once per frame
-    void Update()
-    {
-        if(!EditorApplication.isPlaying){
-            Vector3 currentPostion = transform.position;
-            float snappedX = Mathf.Round(currentPostion.x/tileSize) * tileSize+ tileOffset.x;
-            float snappedZ = Mathf.Round(currentPostion.z/tileSize) * tileSize+ tileOffset.z;
-            float snappedY = tileOffset.y;
-            Vector3 snappedPosition = new Vector3(snappedX,snappedY,snappedZ);
-            transform.position = snappedPosition;
+    //void Update()
+    //{
+    //    if(!EditorApplication.isPlaying){
+    //        Vector3 currentPostion = transform.position;
+    //        float snappedX = Mathf.Round(currentPostion.x/tileSize) * tileSize+ tileOffset.x;
+    //        float snappedZ = Mathf.Round(currentPostion.z/tileSize) * tileSize+ tileOffset.z;
+    //        float snappedY = tileOffset.y;
+    //        Vector3 snappedPosition = new Vector3(snappedX,snappedY,snappedZ);
+    //        transform.position = snappedPosition;
 
 
 
-        }
+    //    }
         
-    }
+    //}
 }
--- Assets/player/Player.cs
+++ Assets/player/Player.cs
@@ -7,18 +7,6 @@ public class Player : MonoBehaviour
     public HashSet<string> backpack = new HashSet<string>();
     public int totalCollectableItemsCount;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public bool checkFullBackpack() {
         return backpack.Count == totalCollectableItemsCount;
     }
